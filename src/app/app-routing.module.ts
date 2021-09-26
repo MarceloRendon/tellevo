2af428af288c5ab6c68d234d68fa6b6e4c25a522
half-frozen <m.rendondemelo@gmail.com>
2021-09-26 19:20:59 -0300

    nuevo sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'restaurar',
+    loadChildren: () => import('./pages/restaurar/restaurar.module').then( m => m.RestaurarPageModule)
+  },
+  {
+    path: 'choferes',
+    loadChildren: () => import('./pages/choferes/choferes.module').then( m => m.ChoferesPageModule)
+  },
+  {
+    path: 'viaje',
+    loadChildren: () => import('./pages/viaje/viaje.module').then( m => m.ViajePageModule)
   },
 ];
 
